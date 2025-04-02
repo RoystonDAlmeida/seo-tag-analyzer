a17eb0e29a6ce360535b60ace0a1a80c71ded57f
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-04-02 19:59:08 +0530

    Storage file

---
+++ server/storage.ts
@@ -0,0 +1,31 @@
+import { seoRequests, type SeoRequest, type InsertSeoRequest } from "@shared/schema";
+
+export interface IStorage {
+  saveRequest(request: InsertSeoRequest): Promise<SeoRequest>;
+  getRequestsByUrl(url: string): Promise<SeoRequest[]>;
+}
+
+export class MemStorage implements IStorage {
+  private requests: Map<number, SeoRequest>;
+  currentId: number;
+
+  constructor() {
+    this.requests = new Map();
+    this.currentId = 1;
+  }
+
+  async saveRequest(insertRequest: InsertSeoRequest): Promise<SeoRequest> {
+    const id = this.currentId++;
+    const request: SeoRequest = { ...insertRequest, id };
+    this.requests.set(id, request);
+    return request;
+  }
+
+  async getRequestsByUrl(url: string): Promise<SeoRequest[]> {
+    return Array.from(this.requests.values()).filter(
+      (request) => request.url === url
+    );
+  }
+}
+
+export const storage = new MemStorage();
